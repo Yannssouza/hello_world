43d2f3bae0f41a8f43dc499876abf3e388514d88
Yann <yannssouza@outlook.com>
2024-01-11 21:41:47 -0300

    first commit

---
+++ src/main/java/com/generation/hello_world/controller/HelloWorldController.java
@@ -0,0 +1,39 @@
+package com.generation.hello_world.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping()
+public class HelloWorldController {
+
+    @GetMapping("/helloWorld")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/bsms")
+    public List<String> getBSMs() {
+        return Arrays.asList(
+                "Mentalidade de Crescimento",
+                "Orientação ao futuro",
+                "Comunicação",
+                "Atenção aos detalhes",
+                "Trabalho em equipe",
+                "Persistência"
+        );
+    }
+
+    @GetMapping("/objetivos")
+    public List<String> getLearningObjectives() {
+        return Arrays.asList(
+                "Aprender Spring Boot",
+                "Entender endpoints em Spring",
+                "Praticar desenvolvimento web com Spring"
+        );
+    }
+}
